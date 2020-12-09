192c6f391a5d960f94051f2cb03aa5c1e74e0373
Alejandro Giorlando <alejandrogiorlando@gmail.com>
2020-12-09 15:30:45 -0300

    Fisrt upload SmrtCoin by aleGio

---
+++ contracts/smrtCoin.sol
@@ -0,0 +1,33 @@
+//pragma
+pragma solidity ^0.6.12;
+
+//imports
+
+
+//clase --> contracts
+/*
+    - Contrato para alamacenar los blances de smrtCoin
+ */
+
+ contract smrtCoin {
+
+    //variables
+    mapping (address => uint) balances;
+
+    //constructor
+    constructor() public{
+        balances[msg.sender] = 1000;
+    }
+
+
+    //Metodos
+    function send(address receiver, uint amount) public {
+        require (balances[msg.sender] > amount);
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
+
+    function getBalance() public view returns(uint){
+        return balances[msg.sender];
+    }
+ }
