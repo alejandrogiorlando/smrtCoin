ab1751267e68e744650f9fc59d75576644ea8015
Alejandro Giorlando <alejandrogiorlando@gmail.com>
2020-12-23 11:33:08 -0300

    fix test - smrtCoin by AleGio

--- test/testSmrtCoin.sol
+++ test/testSmrtCoin.sol
@@ -1,2 +1,18 @@
 //pragma
 pragma solidity ^0.6.12;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddress.sol";
+import "../contracts/smrtCoin.sol";
+
+contract testSmrtCoin{
+
+    function testInitialBalanceOfOwner()public{
+        smrtCoin coin = smrtCoin(DeployedAddress.smrtCoin());
+
+        uint expectedBalance = 1000000;
+
+        Assert.equal(meta.getBalance(tx.origin), expected, 'First account does not have a 1000000 coins');
+    };
+
+}
