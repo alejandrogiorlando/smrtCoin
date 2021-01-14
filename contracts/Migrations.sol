75624eaaadcd348ed9e62b764e726109b56b3147
Alejandro Giorlando <alejandrogiorlando@gmail.com>
2021-01-13 23:49:59 -0300

    change migrations.sol - with TestCoin - smrtCoin by AleGio

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
-
+/*
 contract Migrations {
   address public owner = msg.sender;
   uint public last_completed_migration;
@@ -13,7 +13,25 @@ contract Migrations {
     _;
   }
 
-  function setCompleted(uint completed) public restricted {
+  function setCompleted(uint256 completed) public restricted {
     last_completed_migration = completed;
   }
+}
+/*/
+
+contract Migrations {
+    address public owner;
+    uint256 public last_completed_migration;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
 }
